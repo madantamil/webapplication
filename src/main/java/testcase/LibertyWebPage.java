a6d6eddedc25253749312d910ad1e8cc97b4192a
MADANRAJ THANGARAJ <63949495+madantamil@users.noreply.github.com>
2020-04-19 15:05:32 +0530

    print
    print statement

--- src/main/java/testcase/LibertyWebPage.java
+++ src/main/java/testcase/LibertyWebPage.java
@@ -16,11 +16,7 @@ public class LibertyWebPage {
 	@Test
 	public void execute() {
 
-		System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
-		driver = new InternetExplorerDriver();
-		driver.get("https://www.facebook.com/");
-		driver.manage().window().maximize();
-		driver.close();
+	System.out.println("web page buildong in progress");
 
 	}
 
