6cbad51e708ab8b5d5457448e21421c8810214f9
MADANRAJ THANGARAJ <63949495+madantamil@users.noreply.github.com>
2020-04-19 20:19:17 +0530

    Update LibertyWebPage.java

--- src/main/java/testcase/LibertyWebPage.java
+++ src/main/java/testcase/LibertyWebPage.java
@@ -16,8 +16,8 @@ public class LibertyWebPage {
 
 	@Test
 	public void execute() {
-		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "/src/test/java/chromedriver");
-	//	System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
+	//	System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "/src/test/java/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
 	WebDriver	driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
