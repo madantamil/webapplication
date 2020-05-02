36d5c2943c66a623d68a080e985527ad9710f518
MADANRAJ THANGARAJ <63949495+madantamil@users.noreply.github.com>
2020-05-03 00:33:19 +0530

    Update LibertyWebPage.java

--- src/main/java/testcase/LibertyWebPage.java
+++ src/main/java/testcase/LibertyWebPage.java
@@ -17,7 +17,7 @@ public class LibertyWebPage {
 		driver=new ChromeDriver();
 		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
-		 System.out.println("Welcome To gocd"); 
+		 System.out.println("Welcome To gocd by thoughtworks"); 
 	}
 		}
 
